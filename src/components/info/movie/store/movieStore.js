a1e12dc17128e8ab808b3329c1dcd2e3b55ae72f
Mei <179627138@qq.com>
2017-11-21 11:43:53 +0800

    添加movieStore

---
+++ src/components/info/movie/store/movieStore.js
@@ -0,0 +1,13 @@
+export default new Vuex.Store({
+    state: {
+        reset(state){
+           console.log('in'); 
+        }
+    },
+    mutations: {
+        
+    },
+    actions: {
+
+    }
+})
--- src/components/store/store.js
+++ src/components/store/store.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 import scheduleStore from "../info/schedule/scheduleStore.js"
+import scheduleStore from "../info/movie/store/movieStore.js"
+
 export default new Vuex.Store({
 	modules:{
-		scheduleStore
+		scheduleStore,
+        movieStore
 	}
 })
