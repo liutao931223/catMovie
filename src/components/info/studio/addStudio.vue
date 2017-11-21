a039596851746420dd6a631f7c504eb9abe0f14d
wufuhai <851467665@qq.com>
2017-11-21 11:49:56 +0800

    Revert "wu_addstore"
    This reverts commit 0faa1318894d3842c80e78ecaab2bb2aab6ea13f.

--- src/components/info/studio/addStudio.vue
+++ src/components/info/studio/addStudio.vue
@@ -2,9 +2,7 @@
 	<div>
         <div style="margin-bottom:20px">
             <el-input style="width:400px" v-model="input" placeholder="影院名称..."></el-input>
-        </div>
-        <div style="margin-bottom:20px">
-             <el-input style="width:400px" v-model="input" placeholder="影院地址..."></el-input>
+            <el-input style="width:400px" v-model="input" placeholder="影院地址..."></el-input>
         </div>
         <div>
              <el-button type="primary" round>添加到列表</el-button> 
