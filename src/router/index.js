13bd7c328e8c39a715edc9ae0bcb8839940695e0
Liu Tao <853245267@qq.com>
2017-11-21 10:46:41 +0800

    info 路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,8 @@ import AddMovie from '../components/info/movie/addMovie.vue'
 import MovieList from '../components/info/movie/movieList.vue'
 import AddStudio from '../components/info/studio/addStudio.vue'
 import StudioList from '../components/info/studio/studioList.vue'
-
+import ScheduleList from "../components/info/schedule/scheduleList.vue"
+import AddSchedule from "../components/info/schedule/addSchedule.vue"
 Vue.use(Router)
 
 export default new Router({
@@ -31,6 +32,14 @@ export default new Router({
         {
           path: 'studioList',
           component: StudioList
+        },
+        {
+          path: 'scheduleList',
+          component: ScheduleList
+        },
+        {
+          path: 'addSchedule',
+          component: AddSchedule
         }
       ]
     },{
