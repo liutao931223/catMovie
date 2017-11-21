0faa1318894d3842c80e78ecaab2bb2aab6ea13f
wufuhai <851467665@qq.com>
2017-11-21 11:48:00 +0800

    wu_addstore

--- src/components/info/studio/addStudio.vue
+++ src/components/info/studio/addStudio.vue
@@ -2,7 +2,9 @@
 	<div>
         <div style="margin-bottom:20px">
             <el-input style="width:400px" v-model="input" placeholder="影院名称..."></el-input>
-            <el-input style="width:400px" v-model="input" placeholder="影院地址..."></el-input>
+        </div>
+        <div style="margin-bottom:20px">
+             <el-input style="width:400px" v-model="input" placeholder="影院地址..."></el-input>
         </div>
         <div>
              <el-button type="primary" round>添加到列表</el-button> 
