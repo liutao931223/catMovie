6389284275ef13f6987958b802ad6aa82a076ab5
Mei <179627138@qq.com>
2017-11-21 11:49:56 +0800

    Bug

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 import scheduleStore from "../info/schedule/scheduleStore.js"
-import scheduleStore from "../info/movie/store/movieStore.js"
+import movieStore from "../info/movie/store/movieStore.js"
 
 export default new Vuex.Store({
 	modules:{
