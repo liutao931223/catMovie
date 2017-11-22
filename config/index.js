e5fe39806faef5ed7b6a90fa04cd82baf70059cc
huangruicong <rishtiny@gmail.com>
2017-11-22 09:34:20 +0800

    1

--- config/index.js
+++ config/index.js
@@ -31,7 +31,11 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-        
+       "/users/**": {
+        target: "http://127.0.0.1:3001",
+        secure: false,
+        changeOrigin: true
+      } 
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
